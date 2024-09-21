5dc24df249af16964400efe7d8b48a547d8293a3
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 14:25:30 +0300

    created a submission

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.getElementById('change_button').addEventListener('click', function() {
     }
 });
 
-document.getElementById('reset').addEventListener('click', function() {
+document.getElementById('reset_button').addEventListener('click', function() {
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(item => {
         item.style.backgroundColor = 'transparent';
